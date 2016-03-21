a6667a5f0e53b31e5539964501d78f4ed4017722
Branko Vukelic <branko@outernet.is>
2016-03-21 14:14:31 +0100

    Separated clean_value and added tests
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- confloader.py
+++ confloader.py
@@ -60,6 +60,48 @@ def parse_size(size):
     return size * FACTORS[suffix]
 
 
+def parse_value(val):
+    """ Detect value type and coerce to appropriate Python type
+
+    :param val:     value in configuration format
+    :returns:       python value
+    """
+    # True values: 'yes', 'Yes', 'true', 'True'
+    if val.lower() in ('yes', 'true'):
+        return True
+
+    # False values: 'no', 'No', 'false', 'False'
+    if val.lower() in ('no', 'false'):
+        return False
+
+    # Null values: 'null', 'NULL', 'none', 'None'
+    if val.lower() in ('null', 'none'):
+        return None
+
+    # Floating point numbers: 1.0, 12.443, 1002.3
+    if FLOAT_RE.match(val):
+        return float(val)
+
+    # Integer values: 1, 30, 445
+    if INT_RE.match(val):
+        return int(val)
+
+    # Data sizes: 10B, 12.3MB, 5.6 GB
+    if SIZE_RE.match(val):
+        return parse_size(val)
+
+    # Lists: one item per line, indented
+    if val.startswith('\n'):
+        return [parse_value(v) for v in val[1:].split('\n')]
+
+    # Multi-line string: same as python with triple-doublequotes
+    if val.startswith('"""'):
+        return val.strip('"""').strip()
+
+    # Everything else is returned as is
+    return val
+
+
 class ConfigurationError(Exception):
     """ Raised when application is not configured correctly """
     pass
@@ -114,7 +156,7 @@ class ConfDict(dict):
                     compound_key = key
 
                 if not skip_clean:
-                    value = self.clean_value(value)
+                    value = parse_value(value)
 
                 if section == 'config' and key == 'defaults':
                     default_paths = value
@@ -144,42 +186,3 @@ class ConfDict(dict):
         for k in other:
             if k not in self:
                 self[k] = other[k]
-
-    @staticmethod
-    def clean_value(val):
-        """ Perform coercing of the values """
-
-        # True values: 'yes', 'Yes', 'true', 'True'
-        if val.lower() in ('yes', 'true'):
-            return True
-
-        # False values: 'no', 'No', 'false', 'False'
-        if val.lower() in ('no', 'false'):
-            return False
-
-        # Null values: 'null', 'NULL', 'none', 'None'
-        if val.lower() in ('null', 'none'):
-            return None
-
-        # Floating point numbers: 1.0, 12.443, 1002.3
-        if FLOAT_RE.match(val):
-            return float(val)
-
-        # Integer values: 1, 30, 445
-        if INT_RE.match(val):
-            return int(val)
-
-        # Data sizes: 10B, 12.3MB, 5.6 GB
-        if SIZE_RE.match(val):
-            return parse_size(val)
-
-        # Lists: one item per line, indented
-        if val.startswith('\n'):
-            return val[1:].split('\n')
-
-        # Multi-line string: same as python with triple-doublequotes
-        if val.startswith('"""'):
-            return val.strip('"""').strip()
-
-        # Everything else is returned as is
-        return val
--- tests/test_confloader.py
+++ tests/test_confloader.py
@@ -27,3 +27,63 @@ def test_get_config_path(sys, arg, path):
     sys.argv = ['cmd', '-t', '--flag', '--var="te st"', arg, '--var2=3']
     result = mod.get_config_path()
     assert result == path
+
+
+@pytest.mark.parametrize('conf,val', [
+    ('2B', 2.0),
+    ('2b', 2.0),
+    ('2 b', 2.0),
+    ('200B', 200.0),
+    ('1KB', 1024.0),
+    ('1kb', 1024.0),
+    ('22kb', 22528.0),
+    ('5MB', 5242880.0),
+    ('5mb', 5242880.0),
+    ('5 mb', 5242880.0),
+    ('10GB', 10737418240.0),
+    ('0.3GB', 322122547.2),
+])
+def test_parse_size(conf, val):
+    assert mod.parse_size(conf) == val
+
+
+@pytest.mark.parametrize('conf,val', [
+    ('', ''),
+    ('foo', 'foo'),
+    ('bar', 'bar'),
+    ('yes', True),
+    ('true', True),
+    ('no', False),
+    ('false', False),
+    ('YES', True),
+    ('TRUE', True),
+    ('NO', False),
+    ('FALSE', False),
+    ('Yes', True),
+    ('True', True),
+    ('No', False),
+    ('False', False),
+    ('none', None),
+    ('null', None),
+    ('NONE', None),
+    ('NULL', None),
+    ('None', None),
+    ('Null', None),
+    ('0.3', 0.3),
+    ('12.2', 12.2),
+    ('2', 2),
+    ('0', 0),
+    ('1KB', 1024.0),
+    ('1 kb', 1024.0),
+    ('22kb', 22528.0),
+    ('5MB', 5242880.0),
+    ('5mb', 5242880.0),
+    ('5 mb', 5242880.0),
+    ('10GB', 10737418240.0),
+    ('0.3GB', 322122547.2),
+    ('\nfoo\nbar', ['foo', 'bar']),
+    ('\n1\n2', [1, 2]),
+    ('\nyes\nno', [True, False]),
+])
+def test_clean_value(conf, val):
+    assert mod.parse_value(conf) == val
