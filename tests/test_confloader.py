57add6d274ebfe0e1078f4e189e7ab50cf0b41f2
Branko Vukelic <branko@outernet.is>
2016-03-21 13:31:03 +0100

    Use parametrize instead of iterating dict
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- tests/test_confloader.py
+++ tests/test_confloader.py
@@ -1,22 +1,29 @@
-import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+import pytest
 
 import confloader as mod
 
 
+MOD = mod.__name__
+
+
+@pytest.mark.parametrize('arg,path', [
+    ('--conf=test.ini', 'test.ini'),
+    ("--conf='test.ini'", 'test.ini'),
+    ('--conf="test.ini"', 'test.ini'),
+    ('--conf test.ini', 'test.ini'),
+    ('--conf /path/to/file-test.ini', '/path/to/file-test.ini'),
+    ('--conf=/path/to/file-test.ini', '/path/to/file-test.ini'),
+    ("--conf='/path/to/file-test.ini'", '/path/to/file-test.ini'),
+    ('--conf="/path/to/file-test.ini"', '/path/to/file-test.ini'),
+    ('--conf c,\path\\to\mis leading', 'c,\path\\to\mis'),
+])
 @mock.patch.object(mod, 'sys')
-def test_get_config_path(sys):
-    samples = {
-        '--conf=test.ini': 'test.ini',
-        "--conf='test.ini'": 'test.ini',
-        '--conf="test.ini"': 'test.ini',
-        '--conf test.ini': 'test.ini',
-        '--conf /path/to/file-test.ini': '/path/to/file-test.ini',
-        '--conf=/path/to/file-test.ini': '/path/to/file-test.ini',
-        "--conf='/path/to/file-test.ini'": '/path/to/file-test.ini',
-        '--conf="/path/to/file-test.ini"': '/path/to/file-test.ini',
-        '--conf c:\path\\to\mis leading': 'c:\path\\to\mis',
-    }
-    for (value, expected) in samples.items():
-        sys.argv = ['cmd', '-t', '--flag', '--var="te st"', value, '--var2=3']
-        result = mod.get_config_path()
-        assert result == expected
+def test_get_config_path(sys, arg, path):
+    sys.argv = ['cmd', '-t', '--flag', '--var="te st"', arg, '--var2=3']
+    result = mod.get_config_path()
+    assert result == path
