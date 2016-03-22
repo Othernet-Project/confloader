ebd20974d6a0050c2ab59bf522d4d53c95cbc794
Branko Vukelic <branko@outernet.is>
2016-03-22 20:17:54 +0100

    Fixed classifiers
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- setup.py
+++ setup.py
@@ -20,9 +20,10 @@ setup(
     long_description=read('README.rst'),
     py_modules=[mod.__name__],
     classifiers=[
-        'Development Status :: 1 - Pre Alpha',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
     ],
 )
